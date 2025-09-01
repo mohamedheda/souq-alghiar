73dc7bc62a359fd655ce021a71a22a330bd5ac59
mohamedheda <mohamedheda70@gmail.com>
2025-09-02 01:06:46 +0300

    allow all

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://souq-elghyar.vercel.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
