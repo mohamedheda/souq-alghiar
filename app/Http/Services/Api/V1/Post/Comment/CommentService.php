5395de68332b3a2a215d30ba56b4d7fca1ee0954
mohamedheda <mohamedheda70@gmail.com>
2025-09-13 18:59:21 +0300

    add logic for add comment for user

--- app/Http/Services/Api/V1/Post/Comment/CommentService.php
+++ app/Http/Services/Api/V1/Post/Comment/CommentService.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Services\Api\V1\Post\Comment;
 
+use App\Http\Enums\UserType;
 use App\Http\Services\Api\V1\Post\PostHelperService;
 use App\Http\Traits\Responser;
 use App\Repository\CommentRepositoryInterface;
+use App\Repository\PostRepositoryInterface;
 use Exception;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
@@ -16,6 +18,7 @@ class CommentService
     const REPLY="reply";
     public function __construct(
         private readonly CommentRepositoryInterface $commentRepository ,
+        private readonly PostRepositoryInterface $postRepository ,
         private PostHelperService $helperService ,
     )
     {
@@ -26,6 +29,15 @@ class CommentService
         DB::beginTransaction();
         try {
             $response = Gate::inspect('add-comment', [$request->pinned]);
+            if($request->type == self::COMMENT && auth('api')->user()?->type == UserType::User->value){
+                $post=$this->postRepository->getById($request->parent_id,columns: ['id','user_id']);
+                if($post->user_id!= auth('api')->id())
+                    return $this->responseFail(message: __('messages.you are not authorized to do this action'));
+            }elseif ($request->type == self::REPLY && auth('api')->user()?->type == UserType::User->value){
+                $comment=$this->commentRepository->getById($request->parent_id,columns: ['id','user_id','post_id'],relations: ['post:id,user_id']);
+                if($comment->post?->user_id!= auth('api')->id())
+                    return $this->responseFail(message: __('messages.you are not authorized to do this action'));
+            }
             if ($response->allowed()) {
                 $data = $request->except('type');
                 $data['user_id'] = auth('api')->id();
