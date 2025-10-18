d621d1c09cdd4cd2fc8bfc29206c2f7877a6b2e6
mohamedheda <mohamedheda70@gmail.com>
2025-10-18 19:53:35 +0300

    set limit to user name

--- app/Http/Resources/V1/Product/ProductResource.php
+++ app/Http/Resources/V1/Product/ProductResource.php
@@ -29,7 +29,7 @@ class ProductResource extends JsonResource
             'more_than_main_makes' => $this->moreThanMainMarkes ,
             'user_image' => $this->user?->imageUrl,
             'user_name' => $this->user?->user_name,
-            'name' => Str::limit($this->user?->name,60),
+            'name' => Str::limit($this->user?->name,30),
             'user_id' => $this->user?->id,
             'views' => $this->views,
             'ago_time' => $this->updateAtDiff,
