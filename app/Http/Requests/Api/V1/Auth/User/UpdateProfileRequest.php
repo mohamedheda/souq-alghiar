213190a6cafb9aa2ac1648a222984a654a0928e1
mohamedheda <mohamedheda70@gmail.com>
2025-10-21 21:23:52 +0300

    make email nullable

--- app/Http/Requests/Api/V1/Auth/User/UpdateProfileRequest.php
+++ app/Http/Requests/Api/V1/Auth/User/UpdateProfileRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Api\V1\Auth\User;
 
+use App\Http\Enums\UserType;
 use App\Rules\Phone;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
@@ -29,7 +30,7 @@ class UpdateProfileRequest extends FormRequest
             'user_name' => ['nullable', 'string', 'max:255',Rule::unique('users','user_name')->ignore(auth('api')->id()) ],
             'phone' => ['nullable', 'string', 'max:14',Rule::unique('users','phone')->ignore(auth('api')->id()), new Phone()],
             'address' => ['nullable', 'string'],
-            'email' => ['required', 'email:rfc,dns', Rule::unique('users', 'email')->ignore(auth('api')->id())],
+            'email' => [auth('api')->user()?->type==UserType::Merchant->value ? 'required':'nullable', 'email:rfc,dns', Rule::unique('users', 'email')->ignore(auth('api')->id())],
             'image' => ['nullable', 'image'],
             'cover' => ['nullable', 'image'],
             'city_id' => ['required', Rule::exists('cities', 'id')],
